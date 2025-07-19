007a4ebfd2e134e950bae53f3d0650b4bc6a4665
Pascal Muchiri <pascalmuchiri@gmail.com>
2025-07-19 21:34:34 +0300

    Refactor Supabase configuration to support Deno Deploy; update environment variable handling for project ID and public anon key

--- utils/supabase/info.tsx
+++ utils/supabase/info.tsx
@@ -1,2 +1,16 @@
-export const projectId = process.env.NEXT_PUBLIC_SUPABASE_PROJECT_ID;
-export const publicAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+let projectId: string | undefined;
+let publicAnonKey: string | undefined;
+// @ts-ignore
+if (Deno.env.get('NEXT_PUBLIC_DENO_ENV') === 'true') {
+  // Deno Deploy environment
+  // @ts-ignore
+  projectId = Deno.env.get('NEXT_PUBLIC_SUPABASE_PROJECT_ID');
+  // @ts-ignore
+  publicAnonKey = Deno.env.get('NEXT_PUBLIC_SUPABASE_ANON_KEY');
+} else {
+  // Standard Node/Next.js environment
+  projectId = process.env.NEXT_PUBLIC_SUPABASE_PROJECT_ID;
+  publicAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+}
+
+export { projectId, publicAnonKey };
