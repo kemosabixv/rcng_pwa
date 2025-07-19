c417a1179c162af9c2c0b51473eb304ed04eb50a
Pascal Muchiri <pascalmuchiri@gmail.com>
2025-07-19 22:10:37 +0300

    Simplify Supabase environment variable handling in info.tsx

--- utils/supabase/info.tsx
+++ utils/supabase/info.tsx
@@ -1,22 +1,2 @@
-let projectId: string | undefined;
-let publicAnonKey: string | undefined;
-
-// Use a custom env variable to detect Deno Deploy
-if (
-// @ts-ignore
-  typeof Deno !== 'undefined' &&
-  // @ts-ignore
-  Deno.env.get('NEXT_PUBLIC_DENO_ENV') === 'true'
-) {
-  // Deno Deploy environment
-  // @ts-ignore
-  projectId = Deno.env.get('NEXT_PUBLIC_SUPABASE_PROJECT_ID');
-  // @ts-ignore
-  publicAnonKey = Deno.env.get('NEXT_PUBLIC_SUPABASE_ANON_KEY');
-} else {
-  // Standard Node/Next.js environment
-  projectId = process.env.NEXT_PUBLIC_SUPABASE_PROJECT_ID;
-  publicAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-}
-
-export { projectId, publicAnonKey };
+export const projectId = process.env.NEXT_PUBLIC_SUPABASE_PROJECT_ID;
+export const publicAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
